5c0eb21415760cffd431c318fb5408dc2d294c88
Baskar046 <sselvabaskar046@gmail.com>
2025-07-27 09:48:18 +0530

    list subtraction operation using loop

---
+++ loop_and_sub.py
@@ -0,0 +1,5 @@
+n1=int(input("enter the value of A:"))
+n2=int(input("enter the value of B:"))
+for i in range(1,11):
+    count=n1*i-n2*i         #subtraction operation (-)
+    print(count,end=" ")
