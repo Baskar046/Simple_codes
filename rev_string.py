05e812ad7345ca4b8ddb9836b841f17474b37467
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-08-22 06:39:10 +0530

    Updated new pattern

--- rev_string.py
+++ rev_string.py
@@ -9,3 +9,12 @@ for ch in n:
     result =ch+result
     print(result)
 
+
+#reverse strng using recursive
+def rev_str(n):
+    if not n:
+        return n
+
+    return rev_str(n[1:]) +n[0]
+print(rev_str("hello world"))
+
