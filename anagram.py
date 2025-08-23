343e8b82b36c9bab9fde9d3181d9e102b9245930
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-08-23 06:03:56 +0530

    defintion is added

--- anagram.py
+++ anagram.py
@@ -1,3 +1,8 @@
+"""
+Anagram means (rearranging words) in Python. It's a method to check if two words or phrases are anagrams of each other.
+This is done by seeing if they have the same characters, with the same count for each character, but in a different order. 
+For example, the words "listen" and "silent" are anagrams.
+"""
 def palindrome(s,s1):
     return sorted(s)==sorted(s1)
-print(palindrome("ramar","ramar"))
+print(palindrome("listen","silent"))
