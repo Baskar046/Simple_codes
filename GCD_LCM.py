737b25e2dda884ac80d870f70412b6236e668e2d
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-08-24 07:00:55 +0530

    Update GCD_LCM.py

--- GCD_LCM.py
+++ GCD_LCM.py
@@ -1,10 +1,10 @@
 #GCD
-a=int(input("enter the a value :"))
+a=int(input("enter the a value :"))        
 b=int(input("enter the b value :"))
-small=min(a,b)
+small=min(a,b)               #take the samllest value from a,b 
 for i in range(1,small+1):
-    if a%i==0 and b%i==0:
-        gcd=i
+    if a%i==0 and b%i==0:        #divide by both a and b .which one is satisify both condition .  
+        gcd=i        #stores the gcd value 
 print("gcd of and b is",gcd)
 
 
