1527e77b7ca3c31659c4d1be641c420b13ab89e9
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-08-24 07:06:56 +0530

    Update GCD_LCM.py

--- GCD_LCM.py
+++ GCD_LCM.py
@@ -1,7 +1,7 @@
 #GCD
 a=int(input("enter the a value :"))        
 b=int(input("enter the b value :"))
-small=min(a,b)               #take the samllest value from a,b 
+small=min(a,b)               #take the samllest value from a,b .why we take min value mean takes low time to execute code .
 for i in range(1,small+1):
     if a%i==0 and b%i==0:        #divide by both a and b .which one is satisify both condition .  
         gcd=i        #stores the gcd value 
