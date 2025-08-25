f68473e896bcf9886b4da7cf401ef0dc8b456f05
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-08-25 06:35:23 +0530

    lcm pattern is updated

--- GCD_LCM.py
+++ GCD_LCM.py
@@ -7,4 +7,12 @@ for i in range(1,small+1):
         gcd=i        #stores the gcd value 
 print("gcd of and b is",gcd)
 
-
+#LCM
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+large=max(a,b)        #get a large number 
+while True:
+    if large%a==0 and large%b==0:        
+        break
+    large+=max(a,b)        #increment the large value 
+print("the LCM is",large)
