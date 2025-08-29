b92871c98855e24874299f3cf12b78c786ebc95a
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-08-29 06:08:37 +0530

    Update lambda

--- lambda.py
+++ lambda.py
@@ -1,3 +1,9 @@
+"""
+A lambda expression in Python is a small, anonymous (nameless) function defined with the lambda keyword.
+They are used for short, simple, one-line operations and are often passed as arguments to higher-order functions like map(), filter(), and sorted().
+"""
+
+
 var= lambda a,b : a+b  #lambda expression 
 print(var(1,2))     #pass value 
 
