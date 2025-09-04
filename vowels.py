b23b872ee37ad15cddff061b3994adc6e6ab4ad3
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-09-04 06:33:03 +0530

    vowels

--- vowels.py
+++ vowels.py
@@ -6,3 +6,12 @@ for i in n:
         count+=1
         print(i,end=" ")
 print(count)
+
+
+n="selvabaskar"
+vow=['a','e','i','o','u']
+result=[]
+for i in n:
+    if i in vow:
+        result.append(i)
+print(len(result),result)
