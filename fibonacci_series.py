d50849c693c4582c4a649518e9bccc11b607cbb8
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-09-11 14:15:00 +0530

    Updated new pattern

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -6,3 +6,21 @@ for i in range(1,n+1):
     first_num=second_num
     second_num=third_num
     print("final result",third_num)
+
+
+class fibo:
+    def __init__(self):
+        self.num=n=int(input("enter the number :"))
+
+    def calculation(self):
+        num_1=0
+        num_2=1
+        print(num_1,num_2,end=" ")
+        for i in range(self.num-2):
+            num_3=num_1+num_2
+            num_1=num_2
+            num_2=num_3
+            print(num_3,end=" ")
+
+obj=fibo()
+obj.calculation()
