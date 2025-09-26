fb2c3bafbddc0743482ab32b8adb0adf2294cbb9
SELVA BASKAR S <sselvabaskar046@gmail.com>
2025-09-26 12:37:49 +0530

    Count Days

--- find_n_days.py
+++ find_n_days.py
@@ -2,7 +2,7 @@ class Solution:
     #day =sunday,monday,..
     #n=0,1,2,3,4,...
     def findAnswer(self, d, n):
-       days = {0: "sunday", 1: "monday", 2: "tuesday", 3: "wednesday", 4: "thursday", 5: "friday", 6: "saturday"}
+       days = {0: "sunday", 1: "monday", 2: "tuesday", 3: "wednesday", 4: "thursday", 5: "friday", 6: "saturday"}  #this is refernce not used in code
        print(((d-n)%7))
 
 sol=Solution()
@@ -13,7 +13,18 @@ sol.findAnswer(2,18)
 example: if we give d=2,n=18 means
         formula=(d-n)%7
     solution:
-        (2-18)%7  it gives : -16%7 ,so what is next multiple value of 16 in 7 is 3*7=21.
-        (5-21) : which number gone be reduce the multiplyed from original value is :5
-        output is :5
+        a = b*q + r  , q=quotient , r=remainder
+
+        (d - n) % 7 = (2 - 18) % 7
+             = (-16) % 7
+
+        -16 / 7 = -2.285...
+         floor = -3
+         q = -3
+        
+        r = -16 - 7 * (-3)
+          = -16 + 21
+          = 5
+
+        
 '''
